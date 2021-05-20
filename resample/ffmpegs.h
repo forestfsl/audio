12f15227e4599d04f9d62a5645f333eef53b43ee
fengsonglin <fengsonglin@ad61v1.com>
2021-05-20 21:00:59 +0800

    音频重采样&aac编码实战

---
+++ resample/ffmpegs.h
@@ -0,0 +1,30 @@
+#ifndef FFMPEGS_H
+#define FFMPEGS_H
+
+extern "C" {
+#include <libavformat/avformat.h>
+}
+
+typedef struct{
+    const char *filename;
+    int sampleRate;
+    AVSampleFormat sampleFmt;
+    int chLayout;
+} ResampleAudioSpec;
+
+class FFmpegs
+{
+public:
+    FFmpegs();
+    static void resampleAudio(ResampleAudioSpec &in,ResampleAudioSpec &out);
+    static void resampleAudio(const char *inFilename,
+                              int inSampleRate,
+                              AVSampleFormat inSampleFmt,
+                              int inChLayout,
+                              const char *outFilename,
+                              int outSampleRate,
+                              AVSampleFormat outSampleFmt,
+                              int outChaLayout);
+};
+
+#endif // FFMPEGS_H
